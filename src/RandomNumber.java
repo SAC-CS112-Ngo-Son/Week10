0919f1f21da9f6df5306b2f7df96fc0abd40f8f6
SAC-CS112-Ngo-Son <frank.ngo.1702@gmail.com>
2015-11-03 01:22:44 -0800

    labs

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -21,7 +21,8 @@ public class RandomNumber
 		return rand.nextInt(hi - lo + 1) + lo;
 	}*/
 	
-	private int iLo, iHi;
+	//LAB 4
+	/*private int iLo, iHi;
 	
 	public void SetLowNumber(int low)
 	{
@@ -37,6 +38,44 @@ public class RandomNumber
 	{
 		Random rand = new Random();
 		return rand.nextInt(iHi - iLo + 1) + iLo;
+	}*/
+	
+	//LAB 5
+	/*private int iHigh;
+	
+	public RandomNumber (int high)
+	{
+		iHigh = high;
 	}
 	
+	public int GetANumber()
+	{
+		Random rand = new Random();
+		return rand.nextInt(iHigh + 1);
+	}*/
+	
+	//LAB 6
+	private int iHigh, iLow;
+	
+	public RandomNumber (int low, int high)
+	{
+		iLow = low;
+		iHigh = high;
+	}
+	
+	public int GetANumber()
+	{
+		Random rand = new Random();
+		return rand.nextInt(iHigh - iLow + 1) + iLow;
+	}
+	
+	public int GetLowNumber()
+	{
+		return iLow;
+	}
+	
+	public int GetHighNumber()
+	{
+		return iHigh;
+	}
 }
